c95396e738ab552173d38c171d791c3e6545301b
luknp <luk.npl@gmail.com>
2021-08-18 18:01:02 +0200

    feat: add Material UI, create theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import Routes from 'Routes';
+import { ThemeProvider } from '@material-ui/core/styles';
+import customTheme from 'styles/customTheme';
 
 function App() {
-  return <Routes />;
+  return (
+    <ThemeProvider theme={customTheme(true)}>
+      <Routes />
+    </ThemeProvider>
+  );
 }
-
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { CssBaseline } from '@material-ui/core';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
+      <CssBaseline />
       <App />
     </Router>
   </React.StrictMode>,
