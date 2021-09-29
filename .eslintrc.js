ef73432cffcd734ceedbfd0362c3f47d170bca96
luknp <luk.npl@gmail.com>
2021-09-29 10:47:10 +0200

    feat: main page several approaches (mess)

--- .eslintrc.js
+++ .eslintrc.js
@@ -65,5 +65,12 @@ module.exports = {
       'import/prefer-default-export': 'off',
       '@typescript-eslint/explicit-function-return-type': 'off',
       '@typescript-eslint/no-var-requires': 'off',
+
+      "prettier/prettier": [
+        "error",
+        {
+          "endOfLine": "auto"
+        },
+      ],
     },
   };
